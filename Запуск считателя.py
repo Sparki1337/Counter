c4b1fe1892dd7989d71aab4475e17b26d5d9ea21
Sparki 1337 <cernicenkodana@gmail.com>
2025-06-05 20:47:10 +0300

    Файл запуска

---
+++ Запуск считателя.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+target_directory = r"C:\Users\New\Desktop\Программы\Моё\сурсы\Счётчик для отчётов — копия"
+
+try:
+    os.chdir(target_directory)
+    print(f"Успешно перешли в директорию: {os.getcwd()}")
+    
+    print("Запускаем bot.py...")
+    subprocess.run("python bot.py", shell=True)
+    
+except FileNotFoundError:
+    print(f"Ошибка: Директория не найдена: {target_directory}")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
+
+input("Нажмите Enter для продолжения...") 
